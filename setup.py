f7b1aad6385a931e0c4dc2b4fb8b43dd73128e44
eaminov94 <62853888+eaminov94@users.noreply.github.com>
2020-12-23 13:25:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,3 @@ setup(
     },
     entry_points = {'scrapy': ['settings = FB_events.settings']},
 )
-[settings]
-default = FB_events.settings
-
-[deploy]
-#url = http://localhost:6800/
-project = FB_events
